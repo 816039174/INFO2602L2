a4b136b0cee6d6660457b41f4d53b960da215711
Kobe <157504930+816039174@users.noreply.github.com>
2025-02-18 02:03:24 +0000

    Final changes

--- models.py
+++ models.py
@@ -1,26 +1,46 @@
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.sql.expression import func
-from sqlalchemy.exc import IntegrityError
-from werkzeug.security import generate_password_hash
-from app import app
+      from flask_sqlalchemy import SQLAlchemy
+      from sqlalchemy.sql.expression import func
+      from sqlalchemy.exc import IntegrityError
+      from werkzeug.security import generate_password_hash
+      from app import app
 
-db = SQLAlchemy(app)
+      db = SQLAlchemy(app)
 
 
-class User(db.Model):
-  id = db.Column(db.Integer, primary_key=True)
-  username = db.Column(db.String(80), unique=True, nullable=False)
-  email = db.Column(db.String(120), unique=True, nullable=False)
-  password = db.Column(db.String(120), nullable=False)
+      class User(db.Model):
+        id = db.Column(db.Integer, primary_key=True)
+        username = db.Column(db.String(80), unique=True, nullable=False)
+        email = db.Column(db.String(120), unique=True, nullable=False)
+        password = db.Column(db.String(120), nullable=False)
+          #creates a relationship field to get the user's todos
+        todos = db.relationship('Todo', backref='user', lazy=True, cascade="all, delete-orphan")
 
-  def __init__(self, username, email, password):
-    self.username = username
-    self.email = email
-    self.set_password(password)
+        def init(self, username, email, password):
+          self.username = username
+          self.email = email
+          self.set_password(password)
 
-  def set_password(self, password):
-    """Create hashed password."""
-    self.password = generate_password_hash(password, method='scrypt')
+        def set_password(self, password):
+           """Create hashed password."""
+           self.password = generate_password_hash(password, method= 'pbkdf2:sha256')
 
-  def __repr__(self):
-    return f'<User {self.id} {self.username} - {self.email}>'
+        def repr(self):
+              return f'<User {self.id} {self.username} - {self.email}>'
+
+      class Todo(db.Model):
+        id = db.Column(db.Integer, primary_key=True)
+        user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False) #set userid as a foreign key to user.id 
+        text = db.Column(db.String(255), nullable=False)
+        done = db.Column(db.Boolean, default=False)
+
+        def toggle(self):
+          self.done = not self.done
+          db.session.add(self)
+          db.session.commit()
+
+        def init(self, text):
+            self.text = text
+
+        def repr(self):
+
+          return f'<Todo: {self.id} | {self.user.username} | {self.text} | { "done" if self.done else "not done" }>'
--- wsgi.py
+++ wsgi.py
@@ -1,14 +1,81 @@
 import click, sys
-from models import db, User
+from models import db, User, Todo
 from app import app
-from sqlalchemy.exc import IntegrityError
-
+from sqlalchemy.exc import IntegrityError  
 
 @app.cli.command("init", help="Creates and initializes the database")
 def initialize():
   db.drop_all()
   db.init_app(app)
   db.create_all()
+
   bob = User('bob', 'bob@mail.com', 'bobpass')
+  bob.todos.append(Todo('wash car'))
+
+  db.session.add(bob)
+  db.session.commit()
+
   print(bob)
-  print('database intialized')
+  print('Database initialized')
+
+@app.cli.command("get-user", help="Retrieves a User")
+@click.argument('username', default='bob')
+def get_user(username):
+    bob = User.query.filter_by(username=username).first()
+    if not bob:
+        print(f'{username} not found!')
+        return
+    print(bob)
+
+@app.cli.command('get-users')
+def get_users():
+    users = User.query.all()
+    print(users)
+
+@app.cli.command("change-email")
+@click.argument('username', default='bob')
+@click.argument('email', default='bob@mail.com')
+def change_email(username, email):
+    bob = User.query.filter_by(username=username).first()
+    if not bob:
+        print(f'{username} not found!')
+        return
+    bob.email = email
+    db.session.add(bob)
+    db.session.commit()
+    print(bob)
+
+@app.cli.command('create-user')
+@click.argument('username', default='rick')
+@click.argument('email', default='rick@mail.com')
+@click.argument('password', default='rickpass')
+def create_user(username, email, password):
+    newuser = User(username, email, password)
+    try:
+        db.session.add(newuser)
+        db.session.commit()
+    except IntegrityError:
+        db.session.rollback()
+        print("Username or email already taken!")
+    else:
+        print(newuser)
+
+@app.cli.command('delete-user')
+@click.argument('username', default='bob')
+def delete_user(username):
+    bob = User.query.filter_by(username=username).first()
+    if not bob:
+        print(f'{username} not found!')
+        return
+    db.session.delete(bob)
+    db.session.commit()
+    print(f'{username} deleted')
+
+@app.cli.command('get-todos')
+@click.argument('username', default='bob')
+def get_user_todos(username):
+    bob = User.query.filter_by(username=username).first()
+    if not bob:
+        print(f'{username} not found!')
+        return
+    print(bob.todos)
